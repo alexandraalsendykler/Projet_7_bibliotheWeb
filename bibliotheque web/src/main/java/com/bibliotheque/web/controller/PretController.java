695d0bf1a304abdad1737cbd3bbee4df8ed75d17
alexa <alexa@DESKTOP-J22173Q>
2021-03-29 15:37:28 +0200

    changes

--- bibliotheque web/src/main/java/com/bibliotheque/web/controller/PretController.java
+++ bibliotheque web/src/main/java/com/bibliotheque/web/controller/PretController.java
@@ -2,10 +2,6 @@ package com.bibliotheque.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.bibliotheque.web.repository.PretProxy;
 import com.bibliotheque.web.service.PretService;
 
 @Controller
